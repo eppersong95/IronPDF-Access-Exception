63b7eb57e7732eee06aa4f0902837e9c89996bb2
Garrett Epperson <garrett.epperson@priority1inc.com>
2022-04-14 08:53:24 -0500

    update for new bug

--- IronPDF-Blank-Document/Program.cs
+++ IronPDF-Blank-Document/Program.cs
@@ -1,17 +1,41 @@
 ﻿using IronPdf;
+using System.Drawing;
 
 var baseDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
 var fileIdentifier = DateTime.UtcNow.Ticks;
 
-IronPdf.License.LicenseKey = "IRONPDF.PRIORITYONE.IRO191217.6051.28135-B95DEFC8E0-JRGJIQRLFCRCS-TGCHNC6V3HCM-MUYXYHO2SCTP-OMFCKRCZKZIF-OFHVP4JNFCYS-K42PHR-LAAAAAAAATCLEA-UPGRADE-ESQT6I.RENEW.SUPPORT.17.DEC.2023";
+//get license key from email
+IronPdf.License.LicenseKey = "";
 IronPdf.Logging.Logger.EnableDebugging = true;
 IronPdf.Logging.Logger.LogFilePath = $"{baseDirectory}\\Logs\\Logs-{fileIdentifier}.txt";
 IronPdf.Logging.Logger.LoggingMode = IronPdf.Logging.Logger.LoggingModes.All;
 
 
-var pathToPdf = $"{baseDirectory}\\BuggyPdf.pdf";
+var pathToPdf = $"{baseDirectory}\\Access-Exception.pdf";
 var pdfBytes = File.ReadAllBytes(pathToPdf);
-var pdfDocument = new PdfDocument(pdfBytes);
+var pdfDocument = ConvertPdf(pdfBytes);
 
-var outputPath = $"{baseDirectory}\\Output\\Blank-Pdf-{fileIdentifier}.pdf";
+var outputPath = $"{baseDirectory}\\Output\\Access-Exception-{fileIdentifier}.pdf";
 File.WriteAllBytes(outputPath, pdfDocument.BinaryData);
+
+static PdfDocument ConvertPdf(byte[] pdfBytes)
+{
+    var pdf = new PdfDocument(pdfBytes);
+    var pdfList = new List<PdfDocument>();
+
+    var pageNumber = 0;
+    while (pageNumber < pdf.PageCount)
+    {
+        //exception here
+        var bitmapPage = pdf.PageToBitmap(pageNumber);
+
+        var imageToAdd = bitmapPage as Image;
+
+        var convertedPdf = ImageToPdfConverter.ImageToPdf(imageToAdd, IronPdf.Imaging.ImageBehavior.FitToPageAndMaintainAspectRatio);
+        pdfList.Add(convertedPdf);
+
+        pageNumber++;
+    }
+
+    return PdfDocument.Merge(pdfList);
+}
